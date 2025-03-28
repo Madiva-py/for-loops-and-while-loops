1435fc975c9dd531b255699b061d1899acd8b8ce
michael <aoinihonjin@gmail.com>
2025-03-28 18:15:39 +0300

    I did the for of loop

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const Messages =[];
+    for(let name of names){
+        let message = `Thank you, ${name}, for the wonderful ${event} gift!`
+        Messages.push(message)
+    }
+    return Messages;
+}
+console.log (writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+
+
